fc801b0e3e29f8479e9d9e6e718e5bd3e6ea2b96
Felipe Lopes <luizfelipe.tec.info@gmail.com>
2023-02-14 15:59:17 -0300

    installed React Router Dom and Axios

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+import { Home } from "./pages/Home";
+import { Cadastro } from "./pages/Cadastro";
+import { Menubar } from "./pages/Menubar";
+import { Footer } from "./pages/Footer";
 
 
 function App() {
