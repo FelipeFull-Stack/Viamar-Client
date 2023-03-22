a96d45975679f2ac5491165158f01b00ec18580c
Felipe Lopes <luizfelipe.tec.info@gmail.com>
2023-03-22 11:09:21 -0300

    Incrementando Resultado de Erros fetchCadastros() - ExibirCadastroPage/ADMIN

--- src/components/CRUD Cadastro/ExibirCadastro/index.js
+++ src/components/CRUD Cadastro/ExibirCadastro/index.js
@@ -19,7 +19,7 @@ function ExibirCadastro() {
 				setCadastros(response.data);
 			} catch (err) {
 				console.log(`Erro do Front-end em ExibirCadastro: ${err}`);
-				window.alert("Ops... Alguma coisa deu errada, tente novamente mais tarde.");
+				window.alert("Ops... Alguma coisa deu errada no carregamento dos formulários, tente novamente mais tarde.");
 			}
 		}
 		fetchCadastros();
